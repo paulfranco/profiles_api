a9bbe3921066c715e21ef6ae14e5d1f88a0312b3
Paul Franco <paulfranco@me.com>
2018-03-07 01:57:22 -0800

    added permission and authentication classes to user profile

--- src/porfiles_project/profiles_api/views.py
+++ src/porfiles_project/profiles_api/views.py
@@ -4,7 +4,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import serializers
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 from . import models
+from . import permissions
+
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -95,4 +98,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
     
