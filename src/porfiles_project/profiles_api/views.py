6512b3feb588e62e41ef8630b3eb18b2efec63a7
Paul Franco <paulfranco@me.com>
2018-03-07 02:04:43 -0800

    added search filter

--- src/porfiles_project/profiles_api/views.py
+++ src/porfiles_project/profiles_api/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from . import serializers
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
+from . import serializers
 from . import models
 from . import permissions
 
@@ -100,4 +103,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
-    
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
